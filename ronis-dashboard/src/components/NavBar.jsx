81d84637f6efee14b25bf76df3a9b6181741aab0
bbriannab <bribribb.bb@gmail.com>
2024-11-09 15:50:39 -0600

    larger font for navbar links

--- ronis-dashboard/src/components/NavBar.jsx
+++ ronis-dashboard/src/components/NavBar.jsx
@@ -3,7 +3,7 @@ import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
     return (
-        <nav class="flex justify-between font-MostraNuova text-xl bg-white/50 pt-3 pb-3 px-7">
+        <nav class="flex justify-between font-MostraNuova text-2xl bg-white/50 pt-3 pb-3 px-7">
             <img class="h-20" src="https://www.ezcater.com/db_images/show_img/286566"></img>           
             <ul class="flex flex-row space-x-7 place-items-center">
                 <li><NavLink to="/statistics" exact={true} className="hover:text-primary active:text-primary">STATS</NavLink></li>
