9cb606a1818b9fec55df76a55c195cbc9c2a3f88
minim0no <ahmedlol445@tamu.edu>
2024-11-09 14:19:19 -0600

    init routes

---
+++ ronis-dashboard/src/pages/Recommendations.jsx
@@ -0,0 +1,7 @@
+export default function Recommendations() {
+    return (
+        <div className="flex flex-col items-center justify-center h-screen">
+            <h1 className="text-4xl font-bold text-primary">Recommendations</h1>
+        </div>
+    );
+}
---
+++ ronis-dashboard/src/pages/Statistics.jsx
@@ -0,0 +1,7 @@
+export default function Statistics() {
+    return (
+        <div className="flex flex-col items-center justify-center h-screen">
+            <h1 className="text-4xl font-bold text-primary">Statistics</h1>
+        </div>
+    );
+}
