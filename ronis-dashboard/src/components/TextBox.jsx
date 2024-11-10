f31a92c4731983777528cd87be8d9cd50ab291b2
minim0no <ahmedlol445@tamu.edu>
2024-11-10 09:53:00 -0600

    fix merge conflicts

--- ronis-dashboard/src/components/TextBox.jsx
+++ ronis-dashboard/src/components/TextBox.jsx
@@ -1,14 +1,3 @@
-<<<<<<< HEAD
-export default function TextBox({ text }) {
-    return (
-        <div className="bg-white bg-opacity-10 rounded-lg p-4 w-full mt-6">
-            <div className="p-3 text-white bg-[#f8cd83] bg-opacity-80 rounded-lg mb-2 font-bold break-words w-full">
-                {text}
-            </div>
-        </div>
-    );
-}
-=======
 export default function TextBox({ text }) {
     return (
         <div className="bg-white bg-opacity-10 rounded-lg p-4 w-full mt-6">
@@ -18,4 +7,3 @@ export default function TextBox({ text }) {
         </div>
     );
 }
->>>>>>> 29b7ba8d7893c6a007464c764d0eb6f167d7d32f
